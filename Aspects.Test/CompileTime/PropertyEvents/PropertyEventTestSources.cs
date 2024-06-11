fe23905d959a66f35618e2e3040c0d117b3b1f8a
Florian Reischl <florian.reischl@duatec.at>
2024-06-11 17:25:25 +0200

    On implementing unit tests for property changed source generator

---
+++ Aspects.Test/CompileTime/PropertyEvents/PropertyEventTestSources.cs
@@ -0,0 +1,28 @@
+﻿using Aspects.Attributes.Interfaces;
+using System.Reflection;
+
+namespace Aspects.Test.CompileTime.PropertyEvents
+{
+    internal static class PropertyEventTestSources
+    {
+
+        private static IEnumerable<FieldInfo> GetFieldsWith<T>()
+        {
+            var name = typeof(T).FullName;
+            return typeof(PropertyEventTestClass).GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(f => f.CustomAttributes.Any(a => a.AttributeType.FindInterfaces((t, o) => t.FullName == name, null).Length > 0));
+        }
+
+        public static FieldInfo[] FieldsWithINotifyPropertyChangedAttribute
+            = GetFieldsWith<INotifyPropertyChangedAttribute>().ToArray();
+
+        public static FieldInfo[] FieldsWithINotifyPropertyChangingAttribute
+            = GetFieldsWith<INotifyPropertyChangingAttribute>().ToArray();
+
+        public static FieldInfo[] AllAttributedFields
+            = GetFieldsWith<IPropertyEventGenerationAttribute>().ToArray();
+
+        public static FieldInfo[] FieldsWithBothAttributes
+            = GetFieldsWith<INotifyPropertyEventsAttribute>().ToArray();
+    }
+}
