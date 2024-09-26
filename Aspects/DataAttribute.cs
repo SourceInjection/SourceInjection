8f490a4fab49154831652061157e8db5cf57c07d
Florian Reischl <florian.reischl@duatec.at>
2024-09-26 16:09:18 +0200

    Added Data attribute which combines hash code, to string and equals + (TODO constructors)

---
+++ Aspects/DataAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Aspects.Interfaces;
+using System;
+
+namespace Aspects
+{
+    public class DataAttribute : Attribute, IAutoEqualsAttribute, IAutoHashCodeAttribute, IAutoToStringAttribute
+    {
+        public DataAttribute(
+            DataMemberKind dataMemberKind = DataMemberKind.DataMember,
+            NullSafety nullSafety = NullSafety.Auto,
+            BaseCall baseCall = BaseCall.Auto, 
+            bool storeHashCode = false)
+        {
+            DataMemberKind = dataMemberKind;
+            NullSafety = nullSafety;
+            BaseCall = baseCall;
+            StoreHashCode = storeHashCode;
+        }
+
+        public DataMemberKind DataMemberKind { get; }
+
+        public NullSafety NullSafety { get; }
+
+        public BaseCall BaseCall { get; }
+
+        public bool StoreHashCode { get; }
+    }
+}
